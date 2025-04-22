ef92b658536285ba3b95c9b8ae82d2d0bb16732f
ella <105089634+codewithella@users.noreply.github.com>
2025-04-22 18:00:41 -0300

    Update embaralhador.py

--- embaralhador/embaralhador.py
+++ embaralhador/embaralhador.py
@@ -1,53 +1,22 @@
 #Questão 2
 
-
-def embaralhador1(pesos):
-    '''função que recebe uma lista de pesos e baseada nela gera uma lista
-    de acordo uma lógica(encunciado) sobre os pesos dados como entrada.'''
-    tobig=pesos
-    tosmall=pesos
-    list.sort(tobig) #pondo os pesos em ordem crescente
-    list.sort(tosmall, reverse=True) #pondo os pesos em ordem decrescente
-
-    gerada=[]
+def embaralhador2meio(pesos):
+    '''função que recebe uma lista de pesos e baseada nela retorna uma lista
+    com os pesos reorganizados, alternados entre maior e menor - enunciado da questão '''
     
-    for i in range(len(tosmall)):
-        if tosmall[i]<=tobig[i]:
-            list.append(gerada,tosmall[i])
-            list.append(gerada, tobig[i])
-    return gerada
-            
+    crescente=pesos #passando a lista de pesos para outra lista independente
+    list.sort(crescente) #colocando a nova lista, com os pesos, em ordem crescente
+
+    gerada=[] #criando uma lista vazia que armazanará os pesos na organização pedida no enunciado (organização intercalada)
 
 
+    i=0 #inicializando o indicador da iteração em WHILE
+    while i<len(crescente): #limitando o percurso da iteração para todos os índices da lista de pesos ordenada
+        if crescente[i]<crescente[-(i+1)]: 
+            list.append(gerada, crescente[-(i+1)]) 
+            list.append(gerada, crescente[i]) 
+        elif crescente[i]==crescente[-(i+1)]: #caso dos pesos das músicas serem iguais: os pesos não serão repetidos
+            list.append(gerada,crescente[-(i+1)])
+        i=i+1 #atualizando indicador iterativo
 
-
-def embaralhador2(pesos):
-    '''função que recebe uma lista de pesos e baseada nela gera uma lista
-    de acordo uma lógica(encunciado) sobre os pesos dados como entrada.'''
-    crescente=pesos
-    list.sort(crescente) #colocando a lista de pesos em ordem crescente
-
-    gerada=[]
-    
-    i=0 #pensando na iteração já.
-    while i<len(crescente):
-        if crescente[i]<=crescente[-(i+1)]:
-            list.append(gerada, crescente[-(i+1)]) #adicionando música com maior peso primeiro
-            list.append(gerada, crescente[i])
-        i=i+1
-
-    return gerada
-
-
-def embaralhador3(pesos): #versão sem repetição de músicas, não foi especificado no enunciado.
-    crescente = pesos
-    list.sort(crescente)
-    gerada=[0]
-    for i in range(len(crescente)):
-        p=-(i+1)
-        if crescente[i]<crescente[p]:
-            list.append(gerada, crescente[p])
-            list.append(gerada, crescente[i])
-        elif crescente[i]==crescente[p]:
-            list.append(crescente[i])
     return gerada
